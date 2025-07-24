80394a19f77bfcc91e94a376408828d05a8387d3
Nirmalrajaa K <nirmalrajaa@whitehatglobal.org>
2025-07-24 23:30:23 +0530

    Feat: Array to store random numbers between 1-50

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,13 @@
-package org.example
+import kotlin.random.Random
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val first_array = IntArray(50)
+    for (i in 1..50) {
+        val value: Int = Random.nextInt(50)
+        first_array[i-1] = value
     }
+    val size: Int = first_array.size
+    for (i in 0..size-1)
+        println("The value of Array's index ${i} is ${first_array[i]}")
+
 }
